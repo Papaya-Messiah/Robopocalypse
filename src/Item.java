85bac64a5a451e7961f84a6acb30917447d88816
jlassiter0522 <156385288+jlassiter0522@users.noreply.github.com>
2024-11-28 00:33:38 +0000

    item gen

--- src/Item.java
+++ src/Item.java
@@ -1,7 +1,36 @@
-public abstract class Item {
+public class Item
+{
     private String itemName;
-
-    public String getName(){
-        return itemName;
+    private int Str;
+    private int Agi;
+    private int Def;
+    private int Con;
+    private int Health;
+    public Item(String name, int Strength, int Agility, int Defense, int Constitution, int Heal)
+    {
+        this.itemName = name;
+        this.Str = Strength;
+        this.Agi = Agility;
+        this.Def = Defense;
+        this.Con = Constitution;
+        this.Health = Heal;
+    }
+    public int getStr(){
+        return Str;
+    }
+    public int getAgi(){
+        return Agi;
+    }
+    public int getDef(){
+        return Def;
+    }
+    public int getCon(){
+        return Con;
+    }
+    public int getHealth(){
+        return Health;
+    }
+    public void printItemInfo(){
+        System.out.println(itemName + " str: " + Str + " Agi: " + Agi + " Def: " + Def + " Con: " + Con + " Health: " + Health);
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 /*
  * Authors: Luka Wilmink, Charlotte Lyda-Turner, Cole Lassiter
  * Date: 10/14/2024
@@ -16,6 +16,19 @@ public class Runner {
          * Game.getInstance().run()
          * Game.getInstance().save()
          */
-        Game.getInstance().run();
+        //Game.getInstance().run();
+        ItemHub hub = new ItemHub();
+                ArrayList<Item> itemList = new ArrayList<Item>();
+        ItemHub hub = new ItemHub();
+        int count = 1;
+        for(int i = 0; i < 100; i++){
+            itemList.add(hub.generateItem());
+        }
+        for(Item currentItem : itemList){
+            System.out.print(count + " ");
+            currentItem.printItemInfo();
+            count++;
+        }
+
     }
 }

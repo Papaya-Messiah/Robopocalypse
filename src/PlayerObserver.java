fefcfc0220e2d98ad24ae78a8ba2a646cc07fa21
jlassiter0522 <jlassiter0522@gmail.com>
2024-11-30 00:45:49 -0500

    When player is detected as dead the game will ask to save and then close the game.

--- src/PlayerObserver.java
+++ src/PlayerObserver.java
@@ -11,7 +11,8 @@ public class PlayerObserver implements IObserver {
     public void update(Object o) {
         System.out.println("Player event observed!");
     }
-    public void playerDeath(){
+    @Override
+    public void death(){
         System.out.println("Player died.");
         Player.getInstance().savePlayer();
         System.exit(0);
